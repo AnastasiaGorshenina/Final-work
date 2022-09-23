0b9f4546aa4d248502332d68bfe044bd8ffb6a73
Anastasia <nastya-orsk83@mail.ru>
2022-09-23 14:38:54 +0300

    Final work

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+string[] CreateArray(int array)
+{
+    string [] newArray = new string[array];
+    for (int i = 0; i < array; i++)
+        {    
+          Console.Write($"Input {i} element of the array: ");
+          newArray[i] = Console.ReadLine();    
+        }
+    return newArray;
+}
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string [] myArray = CreateArray (a);
+ShowArray(myArray);
+
+
+void PrintArray(string[] myArray)
+{
+    
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if(myArray[i].Length <= 3)
+        
+        Console.Write($"{myArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(myArray);
+
